3813feefdf7155b135d9de42f5aa1bd7649a5517
dakotafabro <dakotafabrocgu@gmail.com>
2022-06-23 12:51:32 -0700

    connected wikistack db

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ app.use(express.static(__dirname + "/public"));
 
 const views = require("./views");
 
+const { db } = require("./models");
+
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send(views.main("hello world"));
+});
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
 });
 
 const PORT = 3000;
